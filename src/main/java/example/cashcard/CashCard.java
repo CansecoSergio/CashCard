7d0c02cd20b4a58dcb8221be1bb40eac04cd36f5
Canseco Sánchez Sergio Augusto <scansecos@banxico.org.mx>
2024-07-05 18:24:50 -0600

    First commit

---
+++ src/main/java/example/cashcard/CashCard.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package example.cashcard;
+
+/**
+ *
+ * @author DGIE - J72 Aprovisionamiento de Tecnología y Datos Económicos
+ */
+record CashCard(Long id, Double amount) {
+    
+}
---
+++ src/test/java/example/cashcard/CashcardJsonTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package example.cashcard;
+
+import java.io.IOException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+/**
+ *
+ * @author DGIE - J72 Aprovisionamiento de Tecnología y Datos Económicos
+ */
+@JsonTest
+public class CashcardJsonTest {
+
+    @Autowired
+    private JacksonTester<CashCard> jsonTester;
+
+    @Test
+    void cashCardSerializationTest() throws IOException {
+        CashCard cashCard = new CashCard(99L, 123.45);
+
+        Assertions.assertThat(jsonTester.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        Assertions.assertThat(jsonTester.write(cashCard)).hasJsonPathNumberValue("@.id");
+        Assertions.assertThat(jsonTester.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        Assertions.assertThat(jsonTester.write(cashCard)).hasJsonPathNumberValue("@.amount");
+        Assertions.assertThat(jsonTester.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        String expected = """
+                          {
+                            "id":99,
+                            "amount":123.45
+                          }
+                          """;
+
+        Assertions.assertThat(jsonTester.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
+        Assertions.assertThat(jsonTester.parseObject(expected).id()).isEqualTo(99);
+        Assertions.assertThat(jsonTester.parseObject(expected).amount()).isEqualTo(123.45);
+    }
+
+}
